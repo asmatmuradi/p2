729844e31e8b5ee476cf267ae6d2497b0d7c96f0
AsmatUllah Muradi <Asmat@AsmatUllahs-MacBook-Pro.local>
2017-09-20 22:35:03 -0400

    changed index.php and other files

--- calculateBillSplit.php
+++ calculateBillSplit.php
@@ -1,3 +1,37 @@
 <?php
+
+if (isset($_GET['submit']) and ! empty($_GET['submit'])) {
 $tabAmount =$_GET['tabAmount'];
-$devisor =$_GET['numberOfSplits'];
+$splitCount =$_GET['numberOfSplits'];
+$tip =$_GET['tip'];
+
+//calculate discount
+$discountType=$_GET['discount'];
+if ($discountType == 'EmployeeDiscount - 10%'){
+  $tabAmount = $tabAmount - ($tabAmount * 0.10);
+} elseif ($discountType== 'StudentDiscount - 5%'){
+  $tabAmount = $tabAmount - ($tabAmount * 0.05);
+}elseif ($discountType=='SeniorCitizenDiscount - 7%'){
+  $tabAmount = $tabAmount - ($tabAmount * 0.07);
+}
+//check the tip method and convert to dollor amount if its by %
+//if (isset($_GET['submit']) and ! empty($_GET['submit'])) {
+    if (isset($_GET['tipBy'])) {
+        $tip = $_GET['tipBy'];
+        if ($tip =='By %'){
+        $tipAmount=  ($tabAmount * $tip)/100;
+
+      }else {
+        $tipAmount = $tip;
+      };
+    }else {
+      $tipAmount = 0;
+    }
+
+/*} else {
+
+}
+*/
+$tabTotal = tabAmount+tipAmount;
+$tabForEachWay = $tabTotal/$splitCount;
+}
--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php require('calculateBillSplit.php') ?>
+<?php require('calculateBillSplit.php'); ?>
 <!DOCTYPE html>
 <html lang="en">
    <head>
@@ -7,35 +7,56 @@
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
       <link href='/css/styles.css' rel='stylesheet'>
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+      <meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
    </head>
    <body>
-      <div class='bill' >
-         <h1>
-         Bill Split</h>
-         <form class="form-horizontal" action="/examples/actions/confirmation.php" method="post">
-            <div class ='btn btn-secondary btn-small'>
-               <label for="tabAmount">Total Tab </label>
-               <input type="text" id="tabAmount" name="tabAmountTotal">
+      <div class="text-center">
+
+         <h1 class="text-center">Bill Split</h1>
+         </div>
+         <form  class="form-horizontal" method="post">
+            <div class ='text-center'>
+              <label for="tabAmount">Total Tab </label>
+              <input type="text" id="tabAmount" name="tabAmountTotal">
             </div>
             <br />
-            <div class ='btn btn-secondary btn-small'>
+            <div class ='text-center'>
                <label for="numberOfSplits" >Split How Many Ways</label>
-               <input type="text" id="numberOfSplits" name="numberOfDivids">
+               <input type="text" id="numberOfSplits" name="numberOfDivids" class="text-right">
             </div>
             <br />
-            <div class ='btn btn-secondary btn-small'>
+            <div class ='text-center'>
                <label for="tip">Tip For Service </label>
-         <form action="">
+
          <input type="radio" name="tipBy" value="By %">By %
-         <input type="radio" name="tipBy" name="By Amount">By Amount
+         <input type="radio" name="tipBy" value="By Amount" checked>By Amount
          <input type="text" id="tip" name="tip">
-         </form>
+
+         </div >
+         <div class='text-center'>
+           <label for="discount">Discount</label>
+           <select class="text-center" id="discount" style="width: 300px;">
+          <option>Does Not Apply</option>
+           <option>EmployeeDiscount - 10%</option>
+           <option>StudentDiscount - 5%</option>
+           <option>SeniorCitizenDiscount - 7%</option>
+
+         </select>
+
          </div>
+         <div class ='text-center'>
          <input type='submit' class ='btn btn-primary btn-small' value='Calculate'>
-         <div class ='btn btn-secondary btn-small'>
-            <h4>Grand Total</h4>
+
+        </div>
+         <br />
+        <hr  />
+         <div class ='text-center'>
+            <h4 >Total Each Way </h4>
          </div>
-         </form>
-      </div>
+     </form>
+
    </body>
 </html>
